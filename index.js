5cac31c0074fbd94e05beead154b691d5f833c1d
NayanSr <nayankbps@gmail.com>
2023-09-23 15:24:53 +0600

    seventyOne_server_six_jwt-verification-token

--- index.js
+++ index.js
@@ -22,6 +22,26 @@ const client = new MongoClient(uri, {
   },
 });
 
+//! vwrification JWT token
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res
+        .status(403)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -48,8 +68,14 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/bookings", async (req, res) => {
-      // console.log(req.query);
+    app.get("/bookings", verifyJWT, async (req, res) => {
+      const decoded = req.decoded;
+      console.log("come back after ferifiaction", decoded);
+
+      if (decoded.email !== req.query.email) {
+        return res.status(403).send({ error: 1, message: "forbidden access" });
+      }
+
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
